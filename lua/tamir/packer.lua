5b53ceef4c671a60940430c3d982949b95d71ee7
tamirre <tamir.d@web.de>
2025-04-07 22:18:37 +0200

    Initial neovim setup

---
+++ lua/tamir/packer.lua
@@ -0,0 +1,29 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {'srcery-colors/srcery-vim', as = 'srcery'}
+
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = ':TSUpdate'
+  }
+  use {'mbbill/undotree'}
+
+  use {'tpope/vim-fugitive'}
+  
+  use({'neovim/nvim-lspconfig'})
+  use({'hrsh7th/nvim-cmp'})
+  use({'hrsh7th/cmp-nvim-lsp'})
+end)
