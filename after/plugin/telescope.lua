c66522c20d7785a4d9a0e25b63ffa2636f4bd242
tamirre <tamir.d@web.de>
2025-04-10 12:27:55 +0200

    Added live grep (install rip-grep for it

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
