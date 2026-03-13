0d3c0ce36ec1368d6f329607be84098ac51d1a08
tamirre <tamir.d@web.de>
2026-03-13 12:58:29 +0100

    addendum: swapped j/k

--- nvim/.config/nvim/lua/tamir/remap.lua
+++ nvim/.config/nvim/lua/tamir/remap.lua
@@ -38,8 +38,8 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
