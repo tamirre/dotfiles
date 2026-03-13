ba70a2753596c3c04719c1953c38125fabd3207c
tamirre <tamir.d@web.de>
2026-03-13 12:56:33 +0100

    Improved mapping for jumping to warnings and errors

--- nvim/.config/nvim/lua/tamir/remap.lua
+++ nvim/.config/nvim/lua/tamir/remap.lua
@@ -36,8 +36,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
